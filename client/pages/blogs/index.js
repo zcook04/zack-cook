dc6177d105d36ed5754b18689a2b47f5a93747b2
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-18 01:02:58 -0400

    Init blog page templating

---
+++ client/pages/blogs/index.js
@@ -0,0 +1,10 @@
+function AllBlogsPage() {
+    return (
+        <div>
+            <h1>Blogs Page</h1>
+            <p>Placeholder text for the main blogs page</p>
+        </div>
+    )
+}
+
+export default AllBlogsPage
