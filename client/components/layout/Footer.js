c8d178d25903ea6b36df34d56de190c2ff747a88
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-19 02:03:13 -0400

    Add content to main page for cycing professions

--- client/components/layout/Footer.js
+++ client/components/layout/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import classes from './Footer.module.css'
+
 function Footer() {
     return (
-        <footer>
-            <p>Footer component placeholder text</p>
-        </footer>
+        <footer className={classes.footer}><div className={classes.wrapper}>
+            <p className={classes.p}>Footer component placeholder text</p>
+        </div></footer>
     )
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,39 @@
-import Head from 'next/head'
+import React, {useState, useEffect} from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const professions = ['full stack web developer', 'network engineer', 'security professional', 'network security engineer', 'developer', 'problem solver', 'professional', 'content creator', 'information systems security professional']
+
+  const [profession, setProfession] = useState(professions[0])
+
+
+  const updateProfession = (i) => {
+    if (i === professions.length -1){
+      setProfession(professions[0])
+    } else {
+      setProfession(professions[i+1])
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => updateProfession(professions.indexOf(profession)), 4500)
+  }, [profession])
+
   return (
     <div className={styles.heroContainer}>
+      <div className={styles.heroTextWrap}>
+        <div className={styles.heroTextCentered}>
           <h1 className={styles.heroTitle}>
-            Welcome
+            Hi, I'm Zack
           </h1>
           <p className={styles.heroDescription}>
-            Site is under construction.  Please check back later.
+            I'm a <b>{profession}</b>
           </p>
+          <div className={styles.ctadiv}>
+          <p className={styles.cta}>View My Work</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
