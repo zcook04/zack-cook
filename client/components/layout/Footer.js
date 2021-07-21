594b0fa9c191a3633dd93d47b9eb2808c3b9b876
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-20 21:39:35 -0400

    Add social media to footer

--- client/components/layout/Footer.js
+++ client/components/layout/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import Facebook from '../social-icons/Facebook'
+import Linkedin from '../social-icons/Linkedin'
+import Youtube from '../social-icons/Youtube'
+import Twitter from '../social-icons/Twitter'
 
 import classes from './Footer.module.css'
 
-function Footer() {
+function Footer() {        const color = "lightBLue"
     return (
         <footer className={classes.footer}><div className={classes.wrapper}>
-            <p className={classes.p}>Footer component placeholder text</p>
+            <div className={classes.social}>
+                <h3>Connect With Me On Social Media</h3>
+                <div className={classes.socialIcons}>
+                    <Facebook height="40px" width="40px"/>
+                    <Linkedin height="40px" width="40px"/>
+                    <Youtube height="40px" width="40px"/>
+                    <Twitter height="40px" width="40px"/>
+                </div>
+            </div>
         </div></footer>
     )
 }
